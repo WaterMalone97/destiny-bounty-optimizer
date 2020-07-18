ccc8310f71efa363a579925b6932a5335f8becb0
WaterMalone97 <rpm5385@psu.edu>
2020-07-17 20:06:05 -0400

    Send HTML File

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const ServiceContext = require('./ServiceContext');
+var path = require('path');
 
 app.use(express.json());
 app.set('port', (process.env.PORT || 8080));
