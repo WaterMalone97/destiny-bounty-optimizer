16a819bff04b1d08c5d4f24033c09099146b8ca6
WaterMalone97 <rpm5385@psu.edu>
2020-07-17 21:53:37 -0400

    Run Test

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const ServiceContext = require('./ServiceContext');
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.use(express.json());
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 5000));
 let serviceContext = new ServiceContext();
 
 const guardianRouter = require('./routes/guardian-routes');
@@ -18,5 +22,10 @@ app.use('/guardian', function(req, res, next) {
     req.ctx = serviceContext;
     next();
  }, passThroughRouter);
+
+ // Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+ })
 
 app.listen(app.get('port'), () => console.log('Listening on port', app.get('port')));
