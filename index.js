2a7317f04d254cad1d166f4c17c36b399203d2f6
WaterMalone97 <rpm5385@psu.edu>
2020-07-28 20:30:33 -0400

    Added code for login

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+const request = require('request');
 const express = require('express');
 const app = express();
 const path = require('path');
 const ServiceContext = require('./ServiceContext');
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.json());
 app.set('port', (process.env.PORT || 5000));
@@ -12,20 +14,30 @@ let serviceContext = new ServiceContext();
 
 const guardianRouter = require('./routes/guardian-routes');
 const passThroughRouter = require('./routes/passthrough-routes');
+const devRouter = require ('./routes/dev-routes');
 
 app.use('/guardian', function(req, res, next) {
     req.ctx = serviceContext;
     next();
  }, guardianRouter);
 
+ app.use('/dev', function(req, res, next) {
+   req.ctx = serviceContext;
+   next();
+}, devRouter);
+
  app.use('/passthrough', function(req, res, next) {
     req.ctx = serviceContext;
     next();
  }, passThroughRouter);
 
- // Anything that doesn't match the above, send back the index.html file
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname + '/client/build/index.html'))
- })
+//  // Anything that doesn't match the above, send back the index.html file
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+//  })
+
+ app.get('/callback', (req, res) => {
+   
+ });
 
 app.listen(app.get('port'), () => console.log('Listening on port', app.get('port')));
---
+++ routes/dev-routes.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = express.Router();
+const querystring = require('querystring');
+const request = require('request');
+
+router.get('/login', (req, res) => {
+    console.log('Received Login Request');
+     res.redirect('https://www.bungie.net/en/oauth/authorize' + querystring.stringify({
+            client_id: process.env.client_id,
+            response_type: 'code',
+            state: process.env.state
+    }));
+});
+
+router.get('/callback', async (req, res) => {
+    let code = req.query.code;
+    let state = req.query.state;
+    if (state !== process.env.state) {
+        console.log('Recieved callback from D2 API with unknown state. Ignoring...');
+    }
+    else {
+        let tokenRequest = {
+            uri: 'https://www.bungie.net/platform/app/oauth/token/',
+            method: 'POST',
+            body: {
+                grant_type: 'authorization_code',
+                code,
+                client_id: process.env.client_id,
+            }
+        }
+
+        try {
+            let tokenResponse = await new Promise((resolve, reject) => {
+                request.post(tokenRequest, (error, response, body) => {
+                    if (response.statusCode === 200) {
+                        resolve(body);
+                    }
+                    else {
+                        console.log('ERROR REQUESTING TOKEN:', error);
+                        reject(response);
+                    }
+                });
+            });
+            console.log(tokenResponse);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+});
+
+module.exports = router;
